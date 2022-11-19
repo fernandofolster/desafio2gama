60b643629da154cdafda8bb889e69afc5e73560e
Fernando Folster <folster88@gmail.com>
2022-11-18 23:11:22 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # desafio2gama
+# desafio2gama
---
+++ js/eventos.js
@@ -0,0 +1,64 @@
+// EVENTOS
+const eventosContainer = document.querySelector('#eventocontainer');
+const url = "https://xp41-soundgarden-api.herokuapp.com";
+
+async function listaEventos() {
+    const response = await fetch(
+      `https://xp41-soundgarden-api.herokuapp.com/events`,
+      {
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }
+    );
+  
+    const eventos = await response.json();
+
+    eventos.forEach( evento => {
+        const cardEventos = `
+        <article class="evento card p-5 m-3">
+        <h2>${evento.name}</h2>
+        <h4>${evento.attractions}</h4>
+        <p>${evento.description}</p>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addReservaModal">
+        reservar ingresso
+      </button>
+        </article>`
+   
+        eventosContainer.innerHTML += cardEventos;
+    });
+  }
+  
+listaEventos();
+
+//MODAL
+const modalReserva = document.getElementById("addReservaModal");
+const formReserva = modalReserva.querySelector("form");
+
+const params = new URLSearchParams(location.search)
+const id = params.get('_id');
+
+formReserva.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const dados = {
+    owner_name: document.getElementById('nome').value,
+    owner_email: document.getElementById('email').value,
+    number_tickets: 1,
+    event_id: id
+    }; 
+
+  fetch(`${url}/bookings/`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dados),
+  })
+    .then(() => {
+      alert("Reserva feita com sucesso");
+      window.location.replace("eventos.html")
+    })
+    .catch((error) => console.log(error.message));
+});
